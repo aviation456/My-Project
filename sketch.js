ee31b9c5876123cbb5b18195168ac3991212c6a6
aviation456 <92864132+aviation456@users.noreply.github.com>
2021-11-18 18:40:12 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,40,40)
+}
+
+function draw() 
+{
+  background(40);
+  if(keyIsDown(UP_ARROW)){          
+    box.y = box.y -2
+
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.y = box.y +2
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.x = box.x -2
+
+  }
+  if(keyIsDown(RIGHT_ARROW)){
+    box.x = box.x +2
+  }
+  drawSprites()
+}
+
+
+
+
